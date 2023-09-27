211de41e6d5c37432abbc82e7bd0234ad62e5fab
AndresBdull <andresbedullpl@gmail.com>
2023-09-27 16:51:11 -0500

    Documentacion del codigo

--- main.py
+++ main.py
@@ -6,36 +6,37 @@ import sqlite3
 import time
 import datetime
 
+# Configuración de intervalo en minutos
 intervalo_minutos = 15
 
 while True:
-
+    # Conexión a la base de datos SQLite
     conexion = sqlite3.connect('bitrix_bot.db')
-    cursor = conexion.cursor()         
+    cursor = conexion.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS leads (id INTEGER PRIMARY KEY, employee TEXT, phone TEXT, call_date TEXT, status TEXT, day TEXT)')
 
-    #Esto recolecta las fechas
+    # Recolecta la fecha actual
     fecha_actual = datetime.datetime.now()
     fecha_formateada = fecha_actual.strftime("%d/%m/%Y")
 
-    #Inicio de sesion en skype
-    slogin = Skype("cs009@skycellular.net","aztecaintvalentina2019")
-    #Perfil Objetivo
- 
+    # Inicio de sesión en Skype
+    slogin = Skype("cs009@skycellular.net", "aztecaintvalentina2019")
+
+    # Configuración del navegador Chrome
     options = webdriver.ChromeOptions()
     options.service_args = ['--executable_path=C:\driver_chrome\chromedriver.exe']
 
-    # Inicio en el login 
+    # Inicio de sesión en Bitrix24
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.bitrix24.co/")
     driver.maximize_window()
 
-    # Encuentra la sona de inicio de sesion
+    # Encuentra la zona de inicio de sesión
     buttonLogin = driver.find_element(By.CLASS_NAME, "portal-auth-bitrix24__user")
     buttonLogin.click()
     time.sleep(1)
 
-    # Boron de ingreso al Login
+    # Botón de ingreso al Login
     buttonLogin = driver.find_element(By.CSS_SELECTOR, "div.bx-ui-button.bx-ui-button_primary")
     buttonLogin.click()
     time.sleep(10)
@@ -43,31 +44,31 @@ while True:
     # Obtiene el contexto de las ventanas
     ventanas = driver.window_handles
 
-    # Cambiar el enfoque a la ventana emergente
-    driver.switch_to.window(ventanas[-1])  
+    # Cambia el enfoque a la ventana emergente
+    driver.switch_to.window(ventanas[-1])
 
-    # Busqueda de campo para correo
+    # Búsqueda del campo para correo
     user = driver.find_element(By.ID, "login")
     user.send_keys("contratosazteca2023@gmail.com")
     time.sleep(1)
 
-    # Envio siguiente 
+    # Envío siguiente
     buttonNext = driver.find_element(By.CLASS_NAME, "b24-network-auth-form-btn")
     buttonNext.send_keys(Keys.ENTER)
     time.sleep(1)
 
-    #Ingreso de la contraseñas
+    # Ingreso de la contraseña
     password = driver.find_element(By.CLASS_NAME, "b24-network-auth-form-field-input")
     password.send_keys('Botanicas2023/isidro')
     time.sleep(1)
 
-    # Envio siguiente para volver a la web principal
+    # Envío siguiente para volver a la web principal
     buttonNext = driver.find_element(By.CLASS_NAME, "b24-network-auth-form-btn")
     buttonNext.click()
     time.sleep(5)
 
     # Vuelve a la ventana principal
-    driver.switch_to.window(ventanas[0])  
+    driver.switch_to.window(ventanas[0])
 
     # Ingreso al listado de cuentas
     buttonAccountsList = driver.find_element(By.CLASS_NAME, "portal-auth-bitrix24__inner")
@@ -83,6 +84,7 @@ while True:
 
     total_leads = {}  # Utiliza un diccionario en lugar de una lista
 
+    # Diccionario de empresas y sus IDs de chat en Skype
     lista_empresas = {
         "FB Secreto Azteca": "19:483ae87960d34b53b17a13738d1a6e8b@thread.skype",
         "FB Virgen Morena": "19:483ae87960d34b53b17a13738d1a6e8b@thread.skype",
@@ -102,26 +104,23 @@ while True:
 
     empresas = list(lista_empresas.keys())
 
-
     for empresa in empresas:
         try:
-
             if empresa in lista_empresas:
-
                 idChat = lista_empresas[empresa]
-
                 contact = slogin.chats[idChat]
 
+                # Busca y filtra registros en Bitrix24
                 inputFilter = driver.find_element(By.ID, 'voximplant_statistic_detail_filter_search')
                 inputFilter.click()
                 time.sleep(2)
 
                 for i in range(5):
                     inputFilter = driver.find_element(By.NAME, 'PORTAL_USER_ID_label')
-                    inputFilter.clear()  # Utiliza .clear() para borrar el campo de búsqueda
+                    inputFilter.clear()
                     inputFilter.send_keys(empresa)
                     time.sleep(3)
-                
+
                     try:
                         btn = driver.find_element(By.XPATH, f'(//div[contains(text(),"{empresa}")])[{i}]')
                         btn.click()
@@ -133,15 +132,13 @@ while True:
                 time.sleep(5)
 
                 try:
-
                     fila_dict = {}
 
                     llamadaDesconocida = True
                     iteradorLlamada = 1
 
                     while llamadaDesconocida:
-
-                        segundo_elemento = driver.find_element(By.XPATH, f'//tr[@class="main-grid-row main-grid-row-body"][{iteradorLlamada}]')
+                        segundo_elemento = driver.find_element(By.XPATH,f'//tr[@class="main-grid-row main-grid-row-body"][{iteradorLlamada}]')
                         elementos_de_fila = segundo_elemento.find_elements(By.TAG_NAME, "td")
 
                         for i, elemento in enumerate(elementos_de_fila):
@@ -152,19 +149,18 @@ while True:
                         claves_deseadas = ["columna_2", "columna_4", "columna_7", "columna_8"]
                         datos_filtrados = {clave: fila_dict[clave] for clave in claves_deseadas}
 
-                        #Impresion de los elementos
-
+                        # Impresión de los elementos
                         consulta = f'SELECT * FROM leads WHERE call_date = "{datos_filtrados["columna_7"]}" AND  employee = "{datos_filtrados["columna_2"]}" '
                         cursor.execute(consulta)
-
                         resultados = cursor.fetchall()
                         print(resultados)
-                        
-                        if resultados == []:
 
+                        if resultados == []:
                             try:
-                                cursor.execute("INSERT INTO leads (employee, phone, call_date, status, day) VALUES (?, ?, ?, ?, ?)",(datos_filtrados["columna_2"], datos_filtrados["columna_4"], datos_filtrados["columna_7"], datos_filtrados["columna_8"],fecha_formateada))
-                                print("SE INSERTA EN BASE DE DATOS ",empresa)
+                                cursor.execute(
+                                    "INSERT INTO leads (employee, phone, call_date, status, day) VALUES (?, ?, ?, ?, ?)",
+                                    (datos_filtrados["columna_2"], datos_filtrados["columna_4"], datos_filtrados["columna_7"], datos_filtrados["columna_8"], fecha_formateada))
+                                print("SE INSERTA EN BASE DE DATOS ", empresa)
 
                                 mensaje = f'Campaña "{datos_filtrados["columna_2"]}"\nNumero Lead "{datos_filtrados["columna_4"]}"\nFecha "{datos_filtrados["columna_7"]}"\nStatus "{datos_filtrados["columna_8"]}"'
 
@@ -172,8 +168,6 @@ while True:
 
                             except:
                                 print('ERROR AL INSERTAR DATOS EN DB')
-                        #    
-                            
                         else:
                             contact.sendMsg(f'NO HAY REGISTROS NUEVOS EN LA CAMPAÑA: {empresa}')
                             llamadaDesconocida = False
